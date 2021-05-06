ed2ddde6eb824cf5a41f210653d0caaa47f7d61f
muskan1117 <muskan017@gmail.com>
2021-05-06 15:58:52 +0530

    add rubberband

--- Slingshot.js
+++ Slingshot.js
@@ -6,8 +6,11 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,29 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200,20);
+        image(this.sling2, 170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            stroke(48,22,8);
+
+            if(pointA.x<220){
+                strokeWeight(8);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                image(this.sling3, pointA.x-30, pointA.y-10,20,30);
+
+            }
+            else{
+                strokeWeight(5);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+10, pointB.y);
+                image(this.sling3, pointA.x+25, pointA.y-10,20,30);
+
+            }
+            
         }
     }
     
